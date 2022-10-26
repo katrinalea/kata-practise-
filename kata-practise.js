5b8b5351f7f4c30de65d2872d858f3c7af6cdf0b
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-10-26 10:18:09 +0100

    scripts added

--- kata-practise.js
+++ kata-practise.js
@@ -15,11 +15,11 @@ if a is smaller than b
 else
 	return b */
 
-function min (a, b){
-    if(a < b){
-        return a
-    } else {
-        return b
-    }
+function min(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
 }
-console.log(min(1, 2))
+console.log(min(1, 2));
